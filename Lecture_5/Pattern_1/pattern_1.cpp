b9f69847210085cb38f2028d7b83720d12fb3fe2
shubhamxdhapola <shubhamdhapola143@gmail.com>
2026-02-16 10:44:36 +0530

    Patterns

---
+++ Lecture_5/Pattern_1/pattern_1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+ 
+int main() {
+
+    int n;
+
+    cout << "Enter N : ";
+    cin >> n;
+
+    for(int i = 0; i < n; i++) cout << "* ";
+
+    return 0;
+}
