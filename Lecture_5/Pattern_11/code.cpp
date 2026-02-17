b6a1ca3c72df83bd6597441d549701eb9aa14b72
shubhamxdhapola <shubhamdhapola143@gmail.com>
2026-02-17 13:48:07 +0530

    Pattern fixed

--- Lecture_5/Pattern_11/code.cpp
+++ Lecture_5/Pattern_11/code.cpp
@@ -9,40 +9,37 @@ int main()
     cout << "Enter N : ";
     cin >> n;
 
-    for (int i = 0; i < n; i++)
-    {
+    for (int i = 0; i < n; i++) {
 
-        for (int j = 0; j < n - i + 1; j++)
-        {
+        for (int j = 0; j < n - i + 1; j++) {
             cout << "  ";
         }
 
-        for (int j = 0; j < i + 1; j++)
-        {
-            if (j % 2 == 0)
-            {
+        for (int j = 0; j < i + 1; j++) {
+            if (j % 2 == 0) {
                 cout << "* ";
-            }
-            else
-            {
+            } else {
                 cout << "! ";
             }
         }
 
-        for (int j = 0; j < i; j++)
-        {
-            if (j % 2 == 0)
-            {
-                cout << "* ";
-            }
-            else
-            {
-                cout << "! ";
+        for (int j = 0; j < i; j++) {
+            if (i % 2 != 0) {
+                if (j % 2 == 0) {
+                    cout << "* ";
+                } else {
+                    cout << "! ";
+                }
+            } else {
+                if (j % 2 == 0) {
+                    cout << "! ";
+                } else {
+                    cout << "* ";
+                }
             }
         }
 
-        for (int j = 0; j < n - i + 1; j++)
-        {
+        for (int j = 0; j < n - i + 1; j++) {
             cout << "  ";
         }
 
