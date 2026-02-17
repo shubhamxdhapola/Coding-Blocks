95de8753adc78f49e01724c34087413da946aab1
shubhamxdhapola <shubhamdhapola143@gmail.com>
2026-02-17 11:39:07 +0530

    New pattern

---
+++ Lecture_5/Pattern_11/code.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int n;
+
+    cout << "Enter N : ";
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+
+        for (int j = 0; j < n - i + 1; j++)
+        {
+            cout << "  ";
+        }
+
+        for (int j = 0; j < i + 1; j++)
+        {
+            if (j % 2 == 0)
+            {
+                cout << "* ";
+            }
+            else
+            {
+                cout << "! ";
+            }
+        }
+
+        for (int j = 0; j < i; j++)
+        {
+            if (j % 2 == 0)
+            {
+                cout << "* ";
+            }
+            else
+            {
+                cout << "! ";
+            }
+        }
+
+        for (int j = 0; j < n - i + 1; j++)
+        {
+            cout << "  ";
+        }
+
+        cout << "\n";
+    }
+    return 0;
+}
